8ed0869746faae89707e9b29c63838317fa5d3ab
Anfebious <ericdati@hotmail.com>
2023-10-18 12:20:21 -0300

    se sube la carpeta con los ejemplos de la segunda clase

---
+++ main.js
@@ -0,0 +1,87 @@
+
+
+
+
+/* let nombre = prompt("ingresa tu nombre") */
+
+//condicion, true, falso
+
+/*
+if(nombre == "carlos"){
+    alert("bienvenido carlos")
+}else{
+    alert("que mira bobo!")
+}
+*/
+
+/*
+let gaston = true
+
+if(gaston){
+    alert("gaston esta en la clase")
+}
+*/
+
+/*
+let nombre = prompt("decime tu nombre").toLowerCase()
+
+if(nombre == "nico"){
+    alert("bienvenido querido " + nombre)
+}else if(nombre == "carlos"){
+    alert("no sos nico, pero sos "+ nombre + " asi que bienvenido")
+}
+else if(nombre == "tomas"){
+    alert(`hola ${nombre}, pongo la pava?`)
+}else{
+    alert("no sos mi amigo")
+}
+*/
+
+
+
+//< > <= >= !=
+
+/*
+let num1 = 5
+
+let num2 = 10
+
+if(num1 < num2){
+    alert("es menor")
+}
+*/
+
+
+// (AND, OR, NOT) &&, ||, !
+
+/*
+let numA = 5
+
+let numB = 10
+
+if(numA < numB && numA <15){
+    alert("todo ok")
+}
+*/
+
+/*
+producto, alguien que compre, precio base, si me conviene o no vender, avisar por mensaje que paso */
+
+/*
+alert("vendo algo, escucho ofertas")
+
+let ofertaBase = 2000
+let ofertaUsuario = prompt("ingresa tu oferta")
+
+if(ofertaUsuario <1000){
+    alert("no papa, sos un rata")
+}else if(ofertaUsuario >=1000 && ofertaUsuario <= 1999){
+    alert("dale flaco, estirate un poco mas")
+}else if(ofertaUsuario >= ofertaBase){
+    alert("te lo vendo gracias por la compra")
+}else{
+    alert("tu oferta no se entendio")
+}
+*/
+
+
